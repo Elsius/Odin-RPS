2a3284b1c269c9cc1646ea9e9e65cc9905c46fe8
Elsius <sourly95@gmail.com>
2022-07-10 20:07:42 -0700

    Cleaning playGame()

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 console.log('run "playGame()" to play rock paper scissors')
-
+let myPick = '';
 function computerPlay() {
     let random = Math.floor(Math.random() * 3) + 1;
     if (random === 1) {
@@ -20,23 +20,30 @@ function computerPlay() {
         else if lose conditions, you lose
         else invalid input
     */
-function playGame() {
-    let myPick = prompt('Rock, Paper, or Scissors?').toLowerCase()
-    let cpuPick = computerPlay()
+function playGame(myPick = prompt('Rock, Paper, or Scissors?').toLowerCase()) {
+    let cpuPick = computerPlay();
+    //Draw condition
     if (myPick == cpuPick) {
         return `You chose ${myPick}. They chose ${cpuPick}. Draw!`
     }
+    //Win condition
     else if (myPick == 'rock' && cpuPick == 'scissors' || 
-            myPick == 'scissors' && cpuPick == 'paper' || 
-            myPick == 'paper' && cpuPick == 'rock') {
-        return (myPick + ' beats ' + cpuPick + '! Winner!')
+        myPick == 'scissors' && cpuPick == 'paper' || 
+        myPick == 'paper' && cpuPick == 'rock') {
+            return (myPick + ' beats ' + cpuPick + '! Winner!')
     }
-    else if (myPick == 'rock' && cpuPick == 'paper' ||
-            myPick == 'paper' && cpuPick == 'scissors' ||
-            myPick == 'scissors' && cpuPick == 'rock'){
-        return (myPick + ' does not beat ' + cpuPick + '. You lose.')
-            }
+    //Lose condition
+    else if (myPick == 'rock' && cpuPick == 'paper' || 
+        myPick == 'paper' && cpuPick == 'scissors' || 
+        myPick == 'scissors' && cpuPick == 'rock'){
+            return (myPick + ' does not beat ' + cpuPick + '. You lose.')
+    }
+    //Invalid input
     else {
         return `What the hell is ${myPick}?!`
     }
+}
+
+for (let i = 0; i < 5; i++){
+    console.log(playGame(computerPlay()))
 }
